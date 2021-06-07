7dbe3e72a3e7735c5575488fb8a919fff5381d81
ni.colara@hotmail.com <ni.colara@hotmail.com>
2021-06-07 09:03:27 -0400

    Primera pantalla

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@ import App from './components/App.js';
 
 document.getElementById('root').appendChild(App());
 
-
-
-
 document.getElementById('buttonStart').addEventListener("click", goToGame, false);
 var containerStepOne = document.getElementById('containerStepOne');
 var containerStepTwo = document.getElementById('containerStepTwo');
