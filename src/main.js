0d65ef4c5a94b192bdaeb00c7a0bd46c62536d4e
magdalenaa sanhueza <magdasanhueza.s@gmail.com>
2021-06-07 09:11:32 -0400

    Primera pantalla

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-import App from './components/App.js';
+import App from "./components/App.js";
 
-document.getElementById('root').appendChild(App());
+document.getElementById("root").appendChild(App());
+
+document
+  .getElementById("buttonStart")
+  .addEventListener("click", goToGame, false);
+var containerStepOne = document.getElementById("containerStepOne");
+var containerStepTwo = document.getElementById("containerStepTwo");
+containerStepTwo.style = "display:none";
+
+function goToGame() {
+  containerStepOne.style = "display:none";
+  containerStepTwo.style = "display:block";
+}
