d68f662dbd752728fb5d786f837872df9a0bd894
magdalenaa sanhueza <magdasanhueza.s@gmail.com>
2021-06-09 11:29:57 -0400

    pantallas 1 y 2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import App from "./components/App.js";
 
-document.getElementById("root").appendChild(App());
-document.getElementById("buttonStart").addEventListener("click", goToGame, false);
-var containerStepOne = document.getElementById("containerStepOne");
-var containerStepTwo = document.getElementById("containerStepTwo");
-containerStepOne.style = "display:none";
+//document.getElementById("root").appendChild(App());
+document.getElementById("buttonStart").addEventListener("click", buttonStart, false);
+//var containerStepOne = document.getElementById("containerStepOne");
+//var containerStepTwo = document.getElementById("containerStepTwo");
+//containerStepOne.style = "display:none";
 
-function goToGame() {
-  containerStepOne.style = "display:none";
-  containerStepTwo.style = "display:block";
+//function goToGame() {
+  //containerStepOne.style = "display:none";
+  //containerStepTwo.style = "display:block";
+//}
+
+
+
+function buttonStart() {
+  let click = "";
+  console.log("funcionboton")
+      document.getElementById("pantalla1").style.display = "none";
+      document.getElementById("pantalla2").style.display = "block";
+  
 }
-
