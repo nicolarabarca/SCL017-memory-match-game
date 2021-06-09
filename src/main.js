edcc17098b8461f03529f8557ed443330b6d8a52
magdalenaa sanhueza <magdasanhueza.s@gmail.com>
2021-06-09 10:46:57 -0400

    segunda pagina

--- src/main.js
+++ src/main.js
@@ -1,15 +1,13 @@
 import App from "./components/App.js";
 
 document.getElementById("root").appendChild(App());
-
-document
-  .getElementById("buttonStart")
-  .addEventListener("click", goToGame, false);
+document.getElementById("buttonStart").addEventListener("click", goToGame, false);
 var containerStepOne = document.getElementById("containerStepOne");
 var containerStepTwo = document.getElementById("containerStepTwo");
-containerStepTwo.style = "display:none";
+containerStepOne.style = "display:none";
 
 function goToGame() {
   containerStepOne.style = "display:none";
   containerStepTwo.style = "display:block";
 }
+
