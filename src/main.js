d5475e6f6a22e78b5f7010aeb009e0e0730883d2
magdalenaa sanhueza <magdasanhueza.s@gmail.com>
2021-06-10 09:14:26 -0400

    pokemones en pantalla

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import App from "./components/App.js";
 
+import pokemones from '../data/pokemon/pokemon.js';
+
 //document.getElementById("root").appendChild(App());
 document.getElementById("buttonStart").addEventListener("click", buttonStart, false);
 //var containerStepOne = document.getElementById("containerStepOne");
@@ -11,12 +13,28 @@ document.getElementById("buttonStart").addEventListener("click", buttonStart, fa
   //containerStepTwo.style = "display:block";
 //}
 
+let cardBase = document.getElementById("cardBase");
+let cards = document.getElementById("cards");
+cardBase.style = "display:none";
+
 
 
 function buttonStart() {
   let click = "";
   console.log("funcionboton")
-      document.getElementById("pantalla1").style.display = "none";
-      document.getElementById("pantalla2").style.display = "block";
-  
+  document.getElementById("pantalla1").style.display = "none";
+  document.getElementById("pantalla2").style.display = "block";
+  //console.log(pokemones);
+  for(let pokemon of pokemones.items){
+    var card = cardBase.cloneNode(true);
+    card.id = pokemon.id;
+    card.children[0].children[0].src = pokemon.image;
+    card.style = "display:block";
+    cards.appendChild(card);
+    //console.log(card.children[0].children[0]);
+    //console.log(pokemon);
+
+  }
+
+
 }
