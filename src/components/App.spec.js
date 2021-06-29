1c219b28dba63ab7cb7e8569da7d069bf8633a68
Nicole Lara <83736326+nicolarabarca@users.noreply.github.com>
2021-06-29 18:29:44 -0400

    Add files via upload

--- src/components/App.spec.js
+++ src/components/App.spec.js
@@ -1,8 +1,87 @@
-import App from './App.js';
+import App from './App.js'
 
-describe('App', () => {
-  it('should render without crashing', () => {
-    const el = App();
-    expect(el instanceof HTMLElement).toBe(true);
+describe("back to front", () => {
+   it("se prueba la funcion que pone la carta boca arriba", () => {
+    document.body.innerHTML  = `<div id="cardBase" class='cardDesing'>
+      <p class="imagesContainer">
+         <img class="frontCard"
+            src="https://fontmeme.com/permalink/210611/f66485b14d6579c58a847f4a39a45026.png">
+         <img class="backCard" src="/assets/pngwing.com.png">
+      </p>
+   </div>`;
+
+      let app = new App();
+     
+      let cardReturn = app.backToFront(document.getElementById("cardBase"));
+  
+      expect(cardReturn.children[0].children[0].className).toBe("backCard");
+      expect(cardReturn.children[0].children[1].className).toBe("frontCard");
+      
+    });
   });
-});
+  describe("front to back", () => {
+    it("se prueba la funcion que pone la carta boca abajo", () => {
+     document.body.innerHTML  = `<div id="cardBase" class='cardDesing'>
+       <p class="imagesContainer">
+          <img class="backCard"
+             src="https://fontmeme.com/permalink/210611/f66485b14d6579c58a847f4a39a45026.png">
+          <img class="frontCard" src="/assets/pngwing.com.png">
+       </p>
+    </div>`;
+ 
+       let app = new App();
+      
+       let cardReturn = app.frontToBack(document.getElementById("cardBase"));
+   
+       expect(cardReturn.children[0].children[0].className).toBe("frontCard");
+       expect(cardReturn.children[0].children[1].className).toBe("backCard");
+       
+     });
+   });
+  
+<<<<<<< HEAD
+
+
+     describe("front to back", () => {
+       it("se prueba la funcion que pone la carta boca abajo", () => {
+        document.body.innerHTML  = `<div id="cardBase" class='cardDesing'>
+          <p class="imagesContainer">
+             <img class="backCard"
+                src="https://fontmeme.com/permalink/210611/f66485b14d6579c58a847f4a39a45026.png">
+             <img class="frontCard" src="/assets/pngwing.com.png">
+          </p>
+       </div>`;
+    
+          let app = new App();
+         
+          let cardReturn = app.frontToBack(document.getElementById("cardBase"));
+      
+          expect(cardReturn.children[0].children[0].className).toBe("frontCard");
+          expect(cardReturn.children[0].children[1].className).toBe("backCard");
+          
+        });
+      });
+=======
+>>>>>>> 07a7387 (final)
+
+
+     describe("front to back", () => {
+       it("se prueba la funcion que pone la carta boca abajo", () => {
+        document.body.innerHTML  = `<div id="cardBase" class='cardDesing'>
+          <p class="imagesContainer">
+             <img class="backCard"
+                src="https://fontmeme.com/permalink/210611/f66485b14d6579c58a847f4a39a45026.png">
+             <img class="frontCard" src="/assets/pngwing.com.png">
+          </p>
+       </div>`;
+    
+          let app = new App();
+         
+          let cardReturn = app.frontToBack(document.getElementById("cardBase"));
+      
+          expect(cardReturn.children[0].children[0].className).toBe("frontCard");
+          expect(cardReturn.children[0].children[1].className).toBe("backCard");
+          
+        });
+      });
+    
